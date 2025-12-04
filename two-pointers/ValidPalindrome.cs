916db3357c615d388c933d46b88ac2aec6295462
Jonah Morgan <github.finished102@passinbox.com>
2025-12-04 13:53:12 -0600

    COMPLETED: Valid Palindrome

--- two-pointers/ValidPalindrome.cs
+++ two-pointers/ValidPalindrome.cs
@@ -2,8 +2,33 @@ using System.Diagnostics;
 
 bool IsPalindrome(string s)
 {
-    return false;
+    var beginPtr = 0;
+    var lastPtr = s.Length - 1;
+
+    while(beginPtr < lastPtr)
+    {
+        while(!char.IsLetterOrDigit(s[beginPtr]))
+        {
+            beginPtr++;
+            if(beginPtr > s.Length - 1) return true;
+        }
+        while(!char.IsLetterOrDigit(s[lastPtr]))
+        {
+            lastPtr--;
+            if(lastPtr < 0) return true;
+        }
+
+        if(char.ToLower(s[beginPtr]) != char.ToLower(s[lastPtr]))
+        {
+            return false;
+        }
+        beginPtr++;
+        lastPtr--;
+    }
+
+    return true;
 }
 
 Debug.Assert(IsPalindrome("Was it a car or a cat I saw?"));
 Debug.Assert(!IsPalindrome("tab a cat"));
+Debug.Assert(IsPalindrome(".,"));
