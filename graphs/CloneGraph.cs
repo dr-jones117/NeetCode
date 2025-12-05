1abd47c0606247684a3e3e3415aec79a5b919cc3
Jonah Morgan <github.finished102@passinbox.com>
2025-12-05 16:35:47 -0600

    COMPLETED: Clone graph

---
+++ graphs/CloneGraph.cs
@@ -0,0 +1,72 @@
+using System.Diagnostics;
+using System;
+
+var head = new Node(1);
+var two = new Node(2);
+var three= new Node(3);
+
+head.neighbors.Add(two);
+two.neighbors.Add(head);
+two.neighbors.Add(three);
+three.neighbors.Add(two);
+
+var sol = new Solution();
+
+var clone = sol.CloneGraph(head);
+return 0;
+
+// Definition for a Node.
+public class Node {
+    public int val;
+    public IList<Node> neighbors;
+
+    public Node() {
+        val = 0;
+        neighbors = new List<Node>();
+    }
+
+    public Node(int _val) {
+        val = _val;
+        neighbors = new List<Node>();
+    }
+
+    public Node(int _val, List<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+
+public class Solution {
+    public Node CloneGraph(Node node)
+    {
+        if(node == null) return null;
+
+        var visited = new Dictionary<int, Node>();
+        var copiedNode = new Node(node.val);
+        visited[node.val] = copiedNode;
+        foreach(var neighbor in node.neighbors)
+        {
+            copiedNode.neighbors.Add(DFS(neighbor, ref visited));
+        }
+
+        return copiedNode;
+    }
+
+    private Node DFS(Node node, ref Dictionary<int, Node> visited)
+    {
+        if(visited.ContainsKey(node.val))
+        {
+            return visited[node.val];
+        }
+
+        var currNode = new Node(node.val);
+        visited[node.val] = currNode;
+
+        foreach(var neighbor in node.neighbors)
+        {
+            currNode.neighbors.Add(DFS(neighbor, ref visited));
+        }
+
+        return currNode;
+    }
+}
