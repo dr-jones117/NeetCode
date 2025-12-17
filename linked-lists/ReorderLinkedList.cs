946dabe791dff0cf0782f164bbdb8c48b118618c
Jonah Morgan <github.finished102@passinbox.com>
2025-12-17 10:45:32 -0600

    COMPLETED: Reorder Linked List

--- linked-lists/ReorderLinkedList.cs
+++ linked-lists/ReorderLinkedList.cs
@@ -7,13 +7,51 @@ var one = new ListNode(1, two);
 var zero = new ListNode(0, one);
 
 var sol = new Solution();
-var reordered = sol.ReorderList(zero);
+sol.ReorderList(zero);
 
 return 0;
 
 public class Solution {
     public void ReorderList(ListNode head) {
-        
+        // edge cases: if the list is empty or only one point long. short curcuit
+        if(head == null || head.next == null) return;
+
+        // we need to start with slow+fast pointers to get our midpoint
+        // and the end of our list
+        ListNode slow = head, fast = head;
+        while(fast.next != null && fast.next.next != null)
+        {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        // we need to reverse the second half of our list
+        var currPtr = slow.next;
+        slow.next = null;
+        ListNode prev = null;
+
+        while(currPtr != null)
+        {
+            var next = currPtr.next;
+            currPtr.next = prev;
+            prev = currPtr;
+            currPtr = next;
+        }
+
+        // prev and head are the heads of our two lists
+        // now we just need to reorder them
+        var currOne = head;
+        var currTwo = prev;
+        while(currOne != null && currTwo != null)
+        {
+            var currOneNext = currOne.next;
+            currOne.next = currTwo;
+            currOne = currOneNext;
+
+            var currTwoNext = currTwo.next;
+            currTwo.next = currOne;
+            currTwo = currTwoNext;
+        }
     }
 }
 
