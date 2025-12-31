4fa6b50c860cbdf264ca4fec88cad287a40e14b8
Jonah Morgan <github.finished102@passinbox.com>
2025-12-31 14:44:17 -0600

    COMPLETED: Add word search

---
+++ tries/DesignAddAndSearchWordDataStructure.cs
@@ -0,0 +1,64 @@
+using System.Security.Cryptography.X509Certificates;
+
+WordDictionary wordDictionary = new WordDictionary();
+wordDictionary.AddWord("day");
+wordDictionary.AddWord("bay");
+wordDictionary.AddWord("may");
+wordDictionary.Search("say"); // return false
+wordDictionary.Search("day"); // return true
+wordDictionary.Search(".ay"); // return true
+wordDictionary.Search("b.."); 
+return 0;
+
+public class WordDictionary {
+    private class TrieNode
+    {
+        public Dictionary<char, TrieNode> children;
+        public bool isEndOfWord;
+        public TrieNode()
+        {
+            children = new Dictionary<char, TrieNode>();
+            isEndOfWord = false;
+        }
+    }
+    private TrieNode root;
+    public WordDictionary() {
+        this.root = new TrieNode();
+    }
+    
+    public void AddWord(string word) {
+        int idx = 0;
+        var currNode = this.root;
+        while(idx < word.Length)
+        {
+            if(!currNode.children.ContainsKey(word[idx]))
+                currNode.children[word[idx]] = new TrieNode();
+
+            currNode = currNode.children[word[idx]];
+            idx++;
+        }
+        currNode.isEndOfWord = true;
+    }
+    
+    public bool Search(string word) {
+        return RecursiveSearch(word, this.root, 0);
+    }
+
+    private bool RecursiveSearch(string word, TrieNode currNode, int start)
+    {
+        int idx = start;
+        while(idx < word.Length)
+        {
+            if(word[idx] == '.')
+                return currNode.children.Any(kvp => RecursiveSearch(word, kvp.Value, idx + 1));
+
+            if(!currNode.children.ContainsKey(word[idx]))
+            {
+                return false;
+            }
+            currNode = currNode.children[word[idx]];
+            idx++;
+        }
+        return currNode.isEndOfWord;
+    }
+}
