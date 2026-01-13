3c426be56982ec614644fa4228f1358690f04e8e
DrJones117 <jonah.allen.morgan@gmail.com>
2026-01-12 21:33:46 -0600

    COMPLETED: intervals - insert interval

---
+++ intervals/InsertInterval.cs
@@ -0,0 +1,35 @@
+#:property PublishAot=false
+using System.Diagnostics;
+
+var sol = new Solution();
+var res = sol.Insert([[1,3],[4,6]], [2,5]);
+Debug.Assert(res[0].SequenceEqual([1,6]));
+
+public class Solution {
+    public int[][] Insert(int[][] intervals, int[] newInterval)
+    {
+        var result = new List<int[]>();
+
+        for(int i = 0; i < intervals.Length; i++)
+        {
+            if(newInterval[1] < intervals[i][0])
+            {
+                result.Add(newInterval);
+                result.AddRange(intervals.AsEnumerable().Skip(i).ToList());
+                return result.ToArray();
+            }
+            else if(newInterval[0] > intervals[i][1])
+            {
+                result.Add(intervals[i]);
+            }
+            else
+            {
+                newInterval[0] = Math.Min(newInterval[0], intervals[i][0]);
+                newInterval[1] = Math.Max(newInterval[1], intervals[i][1]);
+            }
+        }
+
+        result.Add(newInterval);
+        return result.ToArray();
+    }
+}
